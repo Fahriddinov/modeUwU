e9dac3174a09552798ee7a2d91d1a4a84ff30763
Muhammadamin <fahriddinovmuhammadamin34@gmail.com>
2026-01-14 15:15:49 +0500

    Train sound

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-document.getElementById('activate-button').addEventListener('click', function() {
-    const overlay = document.getElementById("uwu-overlay");
-    const sound = document.getElementById("uwu-sound");
-    
-    overlay.style.display = "flex";
-    sound.play();
-    
-    setTimeout(() => {
-        overlay.style.display = "none";
-    }, 3400);
-});
+const uwuBtn = document.getElementById("activate-button");
+const trainBtn = document.getElementById("train-button");
+
+const uwuOverlay = document.getElementById("uwu-overlay");
+const trainOverlay = document.getElementById("train-overlay");
+
+const uwuSound = document.getElementById("uwu-sound");
+const trainSound = document.getElementById("train-sound");
+
+uwuBtn.onclick = () => {
+  uwuOverlay.style.display = "flex";
+  uwuSound.currentTime = 0;
+  uwuSound.play();
+
+  setTimeout(() => {
+    uwuOverlay.style.display = "none";
+  }, 3500);
+};
+
+trainBtn.onclick = () => {
+  trainOverlay.style.display = "flex";
+  trainSound.currentTime = 0;
+  trainSound.play();
+
+  setTimeout(() => {
+    trainOverlay.style.display = "none";
+  }, 4000);
+};
